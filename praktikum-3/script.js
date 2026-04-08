10c2afe90633d444e547208726fb0c232a3ba9cd
Ditt-A <aditya.akkbbar@gmail.com>
2026-04-08 17:56:11 +0700

    add bab 3

---
+++ praktikum-3/script.js
@@ -0,0 +1,7 @@
+const togglePassword = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("password");
+
+togglePassword.addEventListener("click", function () {
+  const isPassword = passwordInput.type === "password";
+  passwordInput.type = isPassword ? "text" : "password";
+});
